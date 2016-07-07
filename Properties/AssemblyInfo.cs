3a9cfad0161578808a5ba933144b371d0c828788
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2016-07-07 14:23:03 +0200

    Initial import

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Windows;
+
+[assembly: AssemblyTitle("SkiaSharp")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SkiaSharp")]
+[assembly: AssemblyCopyright("Copyright 2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyVersion("1.0.*")]
+[assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
---
+++ Window1.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.Text;
+using System.Threading;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using SkiaSharp;
+
+namespace SkiaSharpDemo
+{
+    public partial class Window1 : Window
+    {
+        public Window1()
+        {
+            InitializeComponent();
+            Thread.CurrentThread.CurrentUICulture = new CultureInfo("en-US");
+        }
+        
+        void Button_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                SkiaCreatePdf();
+            }
+            catch (Exception ex)
+            {
+                Debug.Print(ex.Message);
+                Debug.Print(ex.StackTrace);
+            }
+        }
+        
+        private void SkiaCreatePdf()
+        {
+            float width = 800f;
+            float height = 600f;
+            using (var stream = new SKFileWStream ("document.pdf"))
+            {
+                using (var document = SKDocument.CreatePdf (stream)) 
+                {
+                    // the first page
+                    using (var canvas = document.BeginPage (width, height)) 
+                    {
+                        using (var paint = new SKPaint ()) 
+                        {
+                            canvas.DrawText ("...PDF...", 10f, 100f, paint); 
+                            document.EndPage ();
+                        }
+                    }
+                
+                    // the second page
+                    using (var canvas = document.BeginPage (width, height)) 
+                    {
+                        using (var paint = new SKPaint ()) 
+                        {
+                            canvas.DrawText ("...PDF...", 10f, 100f, paint); 
+                            document.EndPage ();
+                        }
+                    }
+                
+                    // all done
+                    document.Close ();
+                }
+            }
+        }
+    }
+}
